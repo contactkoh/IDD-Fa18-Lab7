289cb9d5985cb53acd1c010774bc9bc6492b47d5
kevOh <ho247@cornell.edu>
2018-10-22 12:57:51 -0700

    adding thumbnail resizing

--- pictureServer.js
+++ pictureServer.js
@@ -29,6 +29,13 @@ var Readline = SerialPort.parsers.Readline; // read serial data as lines
 //-- Addition:
 var NodeWebcam = require( "node-webcam" );// load the webcam module
 
+
+//ME: Added imagemagick
+var fs = require('fs')
+  , gm = require('gm').subClass({imageMagick: true});
+var im = require('imagemagick'); 
+
+
 //---------------------- WEBAPP SERVER SETUP ---------------------------------//
 // use express to create the simple webapp
 app.use(express.static('public')); // find pages in public directory
@@ -87,7 +94,7 @@ const parser = new Readline({
 serial.pipe(parser);
 parser.on('data', function(data) {
   console.log('Data:', data);
-  io.emit('server-msg', data);
+  io.emit('server-msg', data);    
 });
 //----------------------------------------------------------------------------//
 
@@ -116,14 +123,34 @@ io.on('connect', function(socket) {
     /// The .replace() function removes all special characters from the date.
     /// This way we can use it as the filename.
     var imageName = new Date().toString().replace(/[&\/\\#,+()$~%.'":*?<>{}\s-]/g, '');
-
+    //var imageName = 'bob';
+    
     console.log('making a making a picture at'+ imageName); // Second, the name is logged to the console.
 
     //Third, the picture is  taken and saved to the `public/`` folder
-    NodeWebcam.capture('public/'+imageName, opts, function( err, data ) {
-    io.emit('newPicture',(imageName+'.jpg')); ///Lastly, the new name is send to the client web browser.
+    NodeWebcam.capture('public/'+ imageName, opts, function( err, data ) {
+    console.log('Just took a picture '+ imageName);
+        
+      
+    //setTimeout(function(){ console.log('Counting seconds...'); }, 3000);
+    
+    im.resize({
+      srcPath: 'public/'+imageName+'.jpg',
+      dstPath: 'public/'+imageName+'-small.jpg',
+      width:   200
+    }, function(err, stdout, stderr){
+      if (err) throw err;
+      console.log('resized Image to fit within 200 wide');
+    });
+
+    setTimeout(function(){ console.log('Counting seconds...'); }, 3000);
+
+    io.emit('newPicture',(imageName+'-small.jpg')); ///Lastly, the new name is send to the client web browser.
     /// The browser will take this new name and load the picture from the public folder.
+    console.log('Just emitted Thumbnail the ' + imageName+'-small.jpg');
+
   });
+
 
   });
   // if you get the 'disconnect' message, say the user disconnected
